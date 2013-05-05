0950745db399fdb6e1648ab593834915b15773b3
SneakyBrian <SneakyBrian@gmail.com>
2013-05-05 02:22:36 +0100

    generates csv report

--- compare.js
+++ compare.js
@@ -5,7 +5,10 @@ var page = require('webpage').create(),
 	address2,
 	format, 
 	filename1,
-	filename2;
+	filename2,
+	reportname,
+	reportstream,
+	reportexists;
 
 if (system.args.length !== 8) {
     console.log('Usage: compare.js Domain1 Domain2 URLPath outputPath format width height');
@@ -13,24 +16,36 @@ if (system.args.length !== 8) {
 } else {    
 	address1 = system.args[1] + system.args[3];
     address2 = system.args[2] + system.args[3];
-    format = system.args[5];
-    page.viewportSize = { width: system.args[6], height: system.args[7] }; 
-	filename1 = system.args[4] + encodeURIComponent(system.args[3]) + "/" + encodeURIComponent(system.args[1]) + "_" + page.viewportSize.width + "x" + page.viewportSize.height + "." + format;
-	filename2 = system.args[4] + encodeURIComponent(system.args[3]) + "/" + encodeURIComponent(system.args[2]) + "_" + page.viewportSize.width + "x" + page.viewportSize.height + "." + format;
-
+    
+	format = system.args[5];
+    
+	page.viewportSize = { width: system.args[6], height: system.args[7] }; 
+	
+	filename1 = system.args[4] + encodeURIComponent(system.args[3]) + "/" + page.viewportSize.width + "x" + page.viewportSize.height + "/" + encodeURIComponent(system.args[1] + system.args[3]) + "." + format;
+	filename2 = system.args[4] + encodeURIComponent(system.args[3]) + "/" + page.viewportSize.width + "x" + page.viewportSize.height + "/" + encodeURIComponent(system.args[2] + system.args[3]) + "." + format;
+	
+	reportname = system.args[4] + "comparison_report.csv";
+	reportexists = fs.exists(reportname);
+	reportstream = fs.open(reportname, "a");
+	//if the report doesn't exist, add the header
+	if(!reportexists){
+		reportstream.writeLine("URL1,URL2,WIDTH,HEIGHT,PASSED");
+	}
+	
 	console.log("comparing " + address1 + " with " + address2);
 	console.log(page.viewportSize.width + "x" + page.viewportSize.height);
 	
 	getPage(address1, filename1, function(result1) {
 		getPage(address2, filename2, function(result2) {
-			if(result1 !== result2){
+			var same = result1 === result2;
+			reportstream.writeLine(address1 + "," + address2 + "," + page.viewportSize.width + "," + page.viewportSize.height + "," + same);
+			reportstream.close();
+			if(!same){
 				console.log("difference detected!")
 				phantom.exit(3);				
 			} else {
 				//cleanup and exit
 				console.log("no difference detected");
-				//fs.remove(filename1);
-				//fs.remove(filename2);
 				phantom.exit();
 			}
 		});
--- run_compare.bat
+++ run_compare.bat
@@ -1,31 +1,10 @@
 @echo off
 
+phantomjs compare.js http://www.google.co.uk http://www.google.com / output/ png 1024 768
+phantomjs compare.js http://www.google.co.uk http://www.google.com / output/ png 768 1024
 phantomjs compare.js http://www.google.co.uk http://www.google.com /adsense/ output/ png 1024 768
-
-IF ERRORLEVEL 3 GOTO difference 
-IF ERRORLEVEL 2 GOTO loadfail 
-IF ERRORLEVEL 1 GOTO usage 
-
-echo COMPARE PASSED OK!!!
-
-goto exit
-
-:difference
-
-echo DIFFERENCE DETECTED!!!
-
-goto exit
-
-:loadfail
-
-echo FAILED TO LOAD URL!!!
-
-goto exit
-
-:usage
-
-echo INCORRECT USAGE!!!
-
-:exit
+phantomjs compare.js http://www.google.co.uk http://www.google.com /adsense/ output/ png 768 1024
+phantomjs compare.js http://www.google.co.uk http://www.google.com /analytics/ output/ png 1024 768
+phantomjs compare.js http://www.google.co.uk http://www.google.com /analytics/ output/ png 768 1024
 
 pause
